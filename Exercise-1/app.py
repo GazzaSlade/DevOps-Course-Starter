e773cf43f718b0d7ec329cf1f60511e1c0571b04
Slade, Gareth <gareth@gareth-slade.co.uk>
2020-10-09 23:11:25 +0100

    1.2 - Display the list of saved todo items

--- Exercise-1/app.py
+++ Exercise-1/app.py
@@ -8,8 +8,11 @@ app.config.from_object('flask_config.Config')
 # Create base route for default web page and define a function to be executed whenever a request hits the route endpoint
 @app.route('/')
 def index():
+    # Define get_items variable to retrieve all items currently saved within the browser session
+    get_items = session.get_items()
     # Return the rendered "index.html" template
-    return render_template("index.html")
+    # Pass the value of get_items into the template as variable items (used by Jinja2)
+    return render_template("index.html", items=get_items)
 
 # Run this app only when this file is executed directly (rather than imported from another file)
 if __name__ == '__main__':

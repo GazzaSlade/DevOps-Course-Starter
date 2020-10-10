bfefb4e672353fab8f9584cc5865298b1ec6caed
Slade, Gareth <gareth@gareth-slade.co.uk>
2020-10-10 17:29:40 +0100

    1.3 - Create new todo items

--- Exercise-1/app.py
+++ Exercise-1/app.py
@@ -5,6 +5,7 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
 # Create base route for default web page and define a function to be executed whenever a request hits the route endpoint
 @app.route('/')
 def index():
@@ -13,6 +14,18 @@ def index():
     # Return the rendered "index.html" template
     # Pass the value of get_items into the template as variable items (used by Jinja2)
     return render_template("index.html", items=get_items)
+
+
+# Create route for adding items to the todo list and function to be executed when HTML form is submitted
+@app.route('/add_item', methods=['POST'])
+def add_item():
+    # Define get_items variable to retrieve HTML form 'add_item_title' textbox value
+    new_item = request.form.get('add_item_title', None)
+    # Add the retrieved value as an item to our browser session
+    session.add_item(new_item)
+    # Finally redirect the user back to the '/' route where the base route function executes again
+    return redirect('/')
+
 
 # Run this app only when this file is executed directly (rather than imported from another file)
 if __name__ == '__main__':

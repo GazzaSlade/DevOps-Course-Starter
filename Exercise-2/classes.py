d0f38d6457a7cf6e0afb1e1d18a4355cedad1884
Slade, Gareth <gareth@gareth-slade.co.uk>
2020-12-02 23:22:47 +0000

    stretch.2.3 - Add description property to Item object

--- Exercise-2/classes.py
+++ Exercise-2/classes.py
@@ -1,8 +1,9 @@
 class Item(object):
-    def __init__(self, id, title, status="To-Do"):
+    def __init__(self, id, title, status="To-Do", description=""):
         self._id = id
         self._status = status
         self._title = title
+        self._description = description
 
     def get_id(self):
         return self._id
@@ -12,3 +13,6 @@ class Item(object):
 
     def get_title(self):
         return self._title
+
+    def get_description(self):
+        return self._description
